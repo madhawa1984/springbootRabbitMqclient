0da0dd2f93cbb0dad8d7b579c757eb5a3c68b054
madhawa <madhawa.chaminda@pearson.com>
2016-03-30 00:08:41 +0530

    Commit GDC sample

--- src/main/java/com/springapp/com/springapp/MessageHandlers/TaskQueHandler.java
+++ src/main/java/com/springapp/com/springapp/MessageHandlers/TaskQueHandler.java
@@ -8,10 +8,10 @@ import org.springframework.amqp.core.Message;
  */
 public class TaskQueHandler {
     public void handleMessage(textMessage message) {
-        System.out.println("message listener is being executed");
-        System.out.println("received message is "+message);
-        System.out.println("id "+message.getId());
-        System.out.println("description "+message.getDescription());
-        System.out.println("operation "+message.getOperation());
+        System.out.println("------------New Message Received--------------------------");
+        System.out.println("id           :- "+message.getId());
+        System.out.println("description  :- "+message.getDescription());
+        System.out.println("operation    :- "+message.getOperation());
+        System.out.println("------------End of New Message--------------------------");
     }
 }
